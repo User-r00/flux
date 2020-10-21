b9b717d781b56c3ed3379f9224a786d59829ce67
r00 <15057809+User-r00@users.noreply.github.com>
2020-10-21 00:15:02 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''A Python script that drives a Tik Tok lamp.'''
+
+from time import sleep
+
+from TikTokAPI import TikTokAPI
+
+
+# The TikTok username to track.
+USERNAME = 'spaceboyr00'
+
+# Create the TikTok api object.
+api = TikTokAPI()
+
+
+def get_user_follower_count(username):
+    '''Get user follower count by name.'''
+    # Get the user's current data from TikTok.
+    user = api.getUserByName(USERNAME)
+
+    # Return the follower count.
+    return user['userInfo']['stats']['followerCount']
+
+
+# Store for follower counts.
+old_follower_count = get_user_follower_count(USERNAME)
+follower_count = get_user_follower_count(USERNAME)
+
+
+while True:
+    # Store the previous follower count.
+    old_follower_count = follower_count
+
+    # Get current follower count.
+    follower_count = get_user_follower_count(USERNAME)
+
+    # Check if there are any new followers.
+    follower_diff = follower_count - old_follower_count
+    if follower_diff > 0:
+        # Cycle lights.
+        if follower_diff == 1:
+            print(f'{follower_diff} new follower.')
+        else:
+            print(f'{follower_diff} new followers.')
+
+    # Wait to prevent slamming the API.
+    sleep(15)
